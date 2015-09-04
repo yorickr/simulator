0a353ccd3670b0f3231a654745f8239c3205aa73
Arthur Brink <arthur.brink1@gmail.com>
2015-09-04 13:52:32 +0200

    First version

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 142);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TIJD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0 s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "POWER";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0 m";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "VERMOGEN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "0 W";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+
+        private delegate void SetTextCallback(string msg);
+
+        private void setTextLbl06(string text)
+        {
+            //label6.ResetText();
+            if (label6.InvokeRequired)
+            {
+                Invoke(new SetTextCallback(setTextLbl06), new object[] { text });
+            }
+            else
+            { label6.Text = text; }
+
+        }
+
+        private void setTextLbl04(string text)
+        {
+            //label6.ResetText();
+            if (label4.InvokeRequired)
+            {
+                Invoke(new SetTextCallback(setTextLbl04), new object[] { text });
+            }
+            else
+            { label4.Text = text; }
+
+        }
+
+        private void setTextLbl02(string text)
+        {
+            //label6.ResetText();
+            if (label2.InvokeRequired)
+            {
+                Invoke(new SetTextCallback(setTextLbl02), new object[] { text });
+            }
+            else
+            { label2.Text = text; }
+
+        }
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort port;
+        private delegate void SetTextDeleg(string data);
+        private int afstand, tijd, vermogen;
+        
+
+        public Form1()
+        {
+            this.afstand = 0;
+            this.tijd = 0;
+            this.vermogen = 0;
+            InitializeComponent();
+            try
+            {
+                port = new SerialPort("COM10");
+
+                port.BaudRate = 9600;
+                port.Parity = Parity.None;
+                port.StopBits = StopBits.One;
+                port.DataBits = 8;
+                port.Handshake = Handshake.None;
+                port.ReadTimeout = 2000;
+                port.WriteTimeout = 500;
+
+                port.DtrEnable = true;
+
+                port.RtsEnable = true;
+
+                port.Open();
+
+                port.DataReceived += DataReceivedHandler;
+
+            }
+            catch (Exception ex)
+            {
+               
+            }
+
+        }
+
+        public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            System.Threading.Thread.Sleep(500);
+            string indata = sp.ReadExisting();
+            // Console.WriteLine("Received");
+            indata = indata.Replace("CM", "").Trim(); 
+            string command = indata.Substring(0, 2);
+
+            //is.BeginInvoke(new SetTextDeleg(Display), new object[] { command });
+            afstand += 100;
+            tijd += 30;
+
+            switch (command)
+            {
+                case "PW":
+                    
+                    vermogen = int.Parse(indata.Replace("PW ", "").Trim());
+                    String verm = "" + vermogen;
+                    setTextLbl06(verm);
+                    port.WriteLine($"{afstand} {vermogen} {tijd}");
+                    break;
+                case "ST":
+                    port.WriteLine($"{afstand} {vermogen} {tijd}");
+                    break;
+                case "RS":
+                    setTextLbl04("" + 0 + " s");
+                    setTextLbl02("" + 0 + " m");
+                    setTextLbl06("" + 0 + ", W");
+                    vermogen = 0;
+                    tijd = 0;
+                    afstand = 0;
+                    break;
+                default:
+                    port.WriteLine("ERROR");
+                    break;
+            }
+
+            
+            setTextLbl04("" + tijd + " s");
+            setTextLbl02("" + afstand + " m");
+            this.BeginInvoke(new SetTextDeleg(Display), new object[] { indata });
+        }
+        
+        
+
+        private void Display(string displayData)
+        {
+            richTextBox1.Text += displayData;
+
+        }
+    }
+    
+}
