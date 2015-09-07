62785db8f5dcb6dc18512d1a2dbae5981aeb7fd5
Arthur Brink <arthur.brink1@gmail.com>
2015-09-07 12:02:14 +0200

    Finished

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -55,7 +61,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 1;
@@ -66,7 +72,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(93, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(21, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "0 s";
             // 
@@ -75,16 +81,16 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 32);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "POWER";
+            this.label3.Text = "Afstand";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(93, 32);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(24, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "0 m";
             // 
@@ -102,15 +108,75 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(93, 55);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(33, 13);
             this.label6.TabIndex = 6;
-            this.label6.Text = "0 W";
+            this.label6.Text = "25 W";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(171, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Snelheid";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(225, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "0 m/s";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "RPM";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(93, 80);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(171, 32);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "kJ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(225, 32);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(13, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -162,6 +228,29 @@
 
         }
 
+        private void setTextLbl08(string text)
+        {
+            //label6.ResetText();
+            if (label8.InvokeRequired)
+            {
+                Invoke(new SetTextCallback(setTextLbl08), new object[] { text });
+            }
+            else
+            { label8.Text = text; }
+
+        }
+        private void setTextLbl10(string text)
+        {
+            //label6.ResetText();
+            if (label10.InvokeRequired)
+            {
+                Invoke(new SetTextCallback(setTextLbl10), new object[] { text });
+            }
+            else
+            { label10.Text = text; }
+
+        }
+
         private void setTextLbl02(string text)
         {
             //label6.ResetText();
@@ -173,6 +262,25 @@
             { label2.Text = text; }
 
         }
+
+        private void setTextLbl12(string text)
+        {
+            //label6.ResetText();
+            if (label12.InvokeRequired)
+            {
+                Invoke(new SetTextCallback(setTextLbl12), new object[] { text });
+            }
+            else
+            { label12.Text = text; }
+
+        }
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication2
@@ -15,14 +15,19 @@ namespace WindowsFormsApplication2
     {
         private SerialPort port;
         private delegate void SetTextDeleg(string data);
-        private int afstand, tijd, vermogen;
+        private int afstand, tijd, vermogen, rpm, uur, min;
+        private double snelheid, kJ;
+        private Thread sim;
+        private Boolean afstandAflopend, tijdAflopend;
         
 
         public Form1()
         {
             this.afstand = 0;
             this.tijd = 0;
-            this.vermogen = 0;
+            this.vermogen = 25;
+            rpm = 0;
+            kJ = 0.0;
             InitializeComponent();
             try
             {
@@ -48,7 +53,12 @@ namespace WindowsFormsApplication2
             catch (Exception ex)
             {
                
+
+
             }
+
+            sim = new Thread(new ThreadStart(this.Simulatie));
+            sim.Start();
 
         }
 
@@ -58,42 +68,165 @@ namespace WindowsFormsApplication2
             System.Threading.Thread.Sleep(500);
             string indata = sp.ReadExisting();
             // Console.WriteLine("Received");
-            indata = indata.Replace("CM", "").Trim(); 
             string command = indata.Substring(0, 2);
 
             //is.BeginInvoke(new SetTextDeleg(Display), new object[] { command });
-            afstand += 100;
-            tijd += 30;
 
+            if (indata.Contains("PW"))
+            {
+                String filter = indata.Replace("ST", "");
+                filter = filter.Replace("CM", "");
+                filter = filter.Replace("ST", "");
+                filter = filter.Replace("\n", "");
+                vermogen = int.Parse(filter.Replace("PW ", "").Trim());
+                setTextLbl06(vermogen + " W");
+                port.WriteLine($"0 {rpm} {(snelheid * 3.6):00.0} {afstand} {vermogen} {(int)kJ} {uur}:{min} 0 ");
+            }
+
+            if (indata.Contains("ST"))
+            {
+                port.WriteLine($"0 {rpm} {(snelheid * 3.6):00.0} {afstand} {vermogen} {(int)kJ} {uur}:{min} 0 ");
+            }
+
+            if (indata.Contains("RS")) {
+                ResetData();
+            }
+
+            if (indata.Contains("PT"))
+            {
+                String filter = indata.Replace("ST", "");
+                filter = filter.Replace("CM", "");
+                filter = filter.Replace("ST", "");
+                filter = filter.Replace("\n", "");
+                int onberekendeTijd = int.Parse(filter.Replace("PT ", "").Trim());
+                ResetData();
+                tijd = (onberekendeTijd % 100) + (onberekendeTijd / 100) * 60;
+                tijdAflopend = true;
+            }
+
+            if (indata.Contains("PD")) {
+                String filter = indata.Replace("ST", "");
+                filter = filter.Replace("CM", "");
+                filter = filter.Replace("ST", "");
+                filter = filter.Replace("\n", "");
+                ResetData();
+                afstand = 100*int.Parse(filter.Replace("PD ", "").Trim());
+                afstandAflopend = true;
+            }
+
+
+            /*
             switch (command)
             {
                 case "PW":
-                    
                     vermogen = int.Parse(indata.Replace("PW ", "").Trim());
                     String verm = "" + vermogen;
-                    setTextLbl06(verm);
-                    port.WriteLine($"{afstand} {vermogen} {tijd}");
+                    setTextLbl06(verm + " W ");
+                    port.WriteLine($"0 {rpm} {(snelheid * 3.6):00.0} {afstand} {vermogen} {(int)kJ} {uur}:{min} 0 ");
                     break;
                 case "ST":
-                    port.WriteLine($"{afstand} {vermogen} {tijd}");
+                    port.WriteLine($"0 {rpm} {(snelheid * 3.6):00.0} {afstand} {vermogen} {(int)kJ} {uur}:{min} 0 ");
                     break;
                 case "RS":
-                    setTextLbl04("" + 0 + " s");
-                    setTextLbl02("" + 0 + " m");
-                    setTextLbl06("" + 0 + ", W");
+                    sim.Abort();
+                    sim.Join();
                     vermogen = 0;
                     tijd = 0;
                     afstand = 0;
+                    rpm = 0;
+                    snelheid = 0;
+                    kJ = 0;
+                    Reset();
+                    sim = new Thread(new ThreadStart(Simulatie));
+                    sim.Start();
                     break;
+                case "PT":
+                    int onberekendeTijd = int.Parse(indata.Replace("PT ", "").Trim());
+                    tijd = (onberekendeTijd % 100) + (onberekendeTijd / 100) * 60;
+                    tijdAflopend = true;
+                    break;
+                case "PD":
+                    afstand = int.Parse(indata.Replace("PD ", "").Trim());
+                    afstandAflopend = true;
+                    break;
+               
                 default:
                     port.WriteLine("ERROR");
                     break;
             }
-
-            
-            setTextLbl04("" + tijd + " s");
-            setTextLbl02("" + afstand + " m");
+            */
             this.BeginInvoke(new SetTextDeleg(Display), new object[] { indata });
+        }
+
+        private void Reset()
+        {
+            setTextLbl02($" {uur:00}:{min:00}.{tijd%60:00}");
+            setTextLbl04("" + afstand  + " m");
+            setTextLbl06("" + vermogen + " W");
+            setTextLbl08($"{snelheid:0.00} m/s");
+            setTextLbl10("" + rpm);
+            setTextLbl12("" + (int)kJ);
+        }
+
+        public void Simulatie()
+        {
+            Random random = new Random();
+            Boolean con = true;
+            while (con)
+            {
+               
+
+                if (snelheid < (7.5 - (vermogen * 0.015)))
+                {
+                    snelheid += (random.Next(100, 300) / 100.0); 
+                } else if ( snelheid > (10.0 - (vermogen * 0.015))){
+                    snelheid += (random.Next(-100, 0) / 100.0);
+                } else
+                {
+                    snelheid += (random.Next(-50, 50) / 100.0);
+                }
+                rpm = (int)(snelheid / 0.125);
+                if (afstandAflopend)
+                {
+                    afstand -= (int)snelheid;
+                } else
+                {
+                    afstand += (int)snelheid;
+                }
+                if (tijdAflopend){
+                    tijd -= 1;
+                }
+                else {
+                    tijd += 1;
+                }
+                uur = tijd / 3600;
+                min = tijd / 60;
+                kJ += (0.5 * 5.0 * (snelheid*snelheid))/1000.0;
+                Reset();
+                if (tijd < 1 || afstand < 1)
+                {
+                    con = false;
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private void ResetData()
+        {
+            sim.Abort();
+            sim.Join();
+            vermogen = 25;
+            tijd = 0;
+            afstand = 0;
+            rpm = 0;
+            snelheid = 0;
+            kJ = 0;
+            afstandAflopend = false;
+            tijdAflopend = false;
+            Reset();
+            sim = new Thread(new ThreadStart(Simulatie));
+            sim.Start();
         }
         
         
